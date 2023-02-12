5e1ba044e7005b30c352509076969468dadefc46
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2023-02-12 19:08:10 +0600

    updated

--- dashboard.js
+++ dashboard.js
@@ -46,15 +46,18 @@ withdrawBtn.addEventListener("click", function () {
   //condition
   if (
     isNaN(withdrawfieldString) ||
-    withdrawField.value <= 0 ||
+    withdrawField.value >= 0 ||
     withdrawField.value == ""
   ) {
     alert("Enter Valid Number");
     withdrawField.value = "";
-  } else if (totalbl.innerText < withdrawField.value) {
-    alert(" Tor bap eto taka nai");
-    withdrawField.value = "";
-  } else {
+  }
+
+  // else if (totalbl.innerText < withdrawField.value) {
+  //   alert(" Tor bap eto taka nai");
+  //   withdrawField.value = "";
+  // }
+  else {
     const totalWithdrawBalance = newWithdrawValue + previousWithdrawValue;
     withdrawbl.innerText = totalWithdrawBalance;
     //
